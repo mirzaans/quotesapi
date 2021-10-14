b446bff68eb9967629183a311b7211d3944a0637
mirzaans <91285981+mirzaans@users.noreply.github.com>
2021-10-14 04:21:35 -0700

    quotes uodated

---
+++ app/Http/Controllers/QuotesController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Quotes;
+use Illuminate\Http\Request;
+
+class QuotesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Quotes::all();
+    }
+
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+            $request->validate([
+
+                'name'=>'required',
+                'category'=>'required'
+            ]);
+            return Quotes::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Quotes::find($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $quote= Quotes::find($id);
+        $quote->update($request->all());
+        return $quote;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Quotes::destroy($id);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+use App\Models\Quotes;  
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuotesController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::get('/quotes', [QuotesController::class, 'index']);
+Route::post('/store', [QuotesController::class, 'store']);
+Route::get('/quotes/{id}', [QuotesController::class, 'show']);
+Route::put('/quotes/{id}', [QuotesController::class, 'update']);
+Route::delete('/quotes/{id}', [QuotesController::class, 'destroy']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
